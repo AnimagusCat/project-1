6fd43780d7df45469bf4a6f874a91013cac84cf5
syahirah <syahirah.suradi@gmail.com>
2019-09-30 16:49:07 +0800

    fixed the order function so correct messages are displayed when run

--- script.js
+++ script.js
@@ -46,8 +46,38 @@ var customers = [
         food: "steak"
     }
 ];
+var chipsCustomer = document.createElement("img");
+chipsCustomer.src = "images/bigman-cropped.png";
+chipsCustomer.classList.add("chipsCustomer");
+//document.getElementById("middle-customer").appendChild(chipsCustomer);
+
+var saladCustomer = document.createElement("img");
+saladCustomer.src = "images/lady-cropped.png";
+saladCustomer.classList.add("saladCustomer");
+//document.getElementById("middle-customer").appendChild(saladCustomer);
+
+var steakCustomer = document.createElement("img");
+steakCustomer.src = "images/darkman-cropped.png";
+steakCustomer.classList.add("steakCustomer");
+//document.getElementById("middle-customer").appendChild(steakCustomer);
+
 //plate alignment at the start
 var plateLine = [customers[0].food, customers[1].food, customers[2].food];
+
+var chips = document.createElement("img");
+chips.classList.add("chips");
+chips.src = "images/fishchips.png";
+document.getElementById("bottom-board1").appendChild(chips);
+
+var greens = document.createElement("img");
+greens.classList.add("greens");
+greens.src = "images/salad.png";
+document.getElementById("bottom-board2").appendChild(greens);
+
+var meat = document.createElement("img");
+meat.classList.add("meat");
+meat.src = "images/steak.png";
+document.getElementById("bottom-board3").appendChild(meat);
 
 //food on plate
 var plateMatch = plateLine[0];
@@ -57,9 +87,21 @@ var plateMatch = plateLine[0];
 var getCustomer = function() {
     var randomNumber = Math.floor(Math.random() * 3);
     customerMatch = customers[randomNumber].food;
-    return;
+       /* if (customerMatch === customers[0].food){
+        document.getElementById("middle-customer").appendChild(chipsCustomer);
+        div.removeChild(chipsCustomer);
+        } else if (customerMatch === customers[1].food) {
+        document.getElementById("middle-customer").appendChild(saladCustomer);
+        div.removeChild(chipsCustomer);
+        } else if (customerMatch === customers[2].food) {
+        document.getElementById("middle-customer").appendChild(steakCustomer);
+        div.removeChild(chipsCustomer);
+        }*/
+    return customerMatch;
 };
 getCustomer();
+
+
 
 
 //generates a new plate to the end of the plateLine array
@@ -80,12 +122,13 @@ var popPlate = function(){
     //deletes plate from plateLine
     var deletePlate = plateLine.shift();
     console.log("deleted plate: " + deletePlate);
-    //console.log("current plateMatch: " + plateMatch);
     console.log("current plateLine: " + plateLine);
 
     newPlateLine();
     plateMatch= plateLine[0]
-}
+};
+
+
 
 //addEventLister - > spacebar to activate function newPlateLine
 //document.addEventLister("keydown", popPlate);
@@ -97,17 +140,17 @@ var popPlate = function(){
 });*/
 
 //checks whether customerMatch and plateMatch are true
-var order = function (customerMatch, plateMatch){
-    if (customerMatch === plateMatch) {
+//check whether it runs without parameters??
+var order = function (plateMatch, customerMatch){
+    if (plateMatch === customerMatch) {
         console.log("correct!");
         points += 10;
         getCustomer();
         popPlate();
-    } else if (customerMatch !== plateMatch) {
+    } else if (plateMatch !== customerMatch) {
         console.log("wrong!");
         fail();
     }
-    return;
 };
 
 var fail = function() {
@@ -116,21 +159,29 @@ var fail = function() {
         strikes++;
     } else if (strike === 3){
         console.log("GAME OVER Strike no. " + strikes);
+        alert("GAME OVER");
         //show option to restart game
     }
     return;
 };
 
+var endGame = function (){
+
+};
 /*var success = function(){
     if (maxTime <= 0) {
         //stop game
         //show option to restart game
     }
 };*/
+/*var startMessage = document.createElement("div");
+startMessage.classList.add("starMessage");
+startMessage.textContent = "Instructions: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+document.appendChild(startMessage);*/
 
-/*var gameStart = function (){
+var gameStart = function (){
     var points = 0;
     var strikes = 0;
     getCustomer();
     startTimer();
-};*/
+};
